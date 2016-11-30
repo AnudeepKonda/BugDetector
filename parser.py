b56a977bf84535b3119dccd8fda65f7c930a892f
Lance Simmons <lancersimmons@gmail.com>
2016-11-30 00:10:53 -0500

    start combining parser files

--- parser.py
+++ parser.py
@@ -108,5 +108,59 @@ def main():
     fileHandler.close()
 
 
+
+    fp = open("tokenLines.txt", "r")
+    data = fp.readlines()
+
+    #fp = open("tokensLines_25.txt")
+    buf = [ ]
+    one_big_line = ' '.join(data)
+    data = one_big_line.split(' ')
+    total_tokens = len(data)
+
+    fp2 = open("tokenLines_100.txt", "w")
+    count = 0
+    while count < total_tokens-100:
+        for i in range(0, 101):
+            if data[count+i].find("\n") != -1:
+                buf.append(data[count+i][:-1])
+            else:
+                buf.append(data[count+i])
+        count = count + 100
+        string_to_write = ' '.join(buf)
+        fp2.write(string_to_write+'\n')
+        #print(count)
+        buf=[ ]
+    fp2.close()
+    fp.close()
+
+
+    ## FIXME: Use this to simplify process?
+
+    # fp = open("tokenLines.txt", "r")
+    # data = fp.readlines()
+    # fp.close()
+
+    # buf = [ ]
+    # one_big_line = ' '.join(data)
+    # data = one_big_line.split(' ')
+    # total_tokens = len(data)
+
+    # fp2 = open("tokenLines.txt", "w")
+    # count = 0
+    # while count < total_tokens-100:
+    #     for i in range(0, 101):
+    #         if data[count+i].find("\n") != -1:
+    #             buf.append(data[count+i][:-1])
+    #         else:
+    #             buf.append(data[count+i])
+    #     count = count + 100
+    #     string_to_write = ' '.join(buf)
+    #     fp2.write(string_to_write+'\n')
+    #     #print(count)
+    #     buf=[ ]
+    # fp2.close()
+
+
 if __name__ == "__main__":
     main()
