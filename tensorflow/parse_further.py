f056f2cd38e60466d260dcf06ad195a5b60dffa4
Lance Simmons <lancersimmons@gmail.com>
2016-11-30 00:01:14 -0500

    added progress indicators

--- tensorflow/parse_further.py
+++ tensorflow/parse_further.py
@@ -1,24 +1,33 @@
-fp = open("tokenLines.txt", "r")
-data = fp.readlines()
+#!/usr/bin/python
+#
+# This script will be used to produce a file with 100 tokens on each line.
 
-#fp = open("tokensLines_25.txt")
-buf = [ ]
-one_big_line = ' '.join(data)
-data = one_big_line.split(' ')
-total_tokens = len(data)
+def main():
 
-fp2 = open("tokenlines_100.txt", "w")
-count = 0
-while count < total_tokens-100:
-	for i in range(0, 101):
-		if data[count+i].find("\n") != -1:
-			buf.append(data[count+i][:-1])
-		else:
-			buf.append(data[count+i])
-	count = count + 100
-	string_to_write = ' '.join(buf)
-	fp2.write(string_to_write+'\n')
-	#print(count)
-	buf=[ ]
-fp2.close()
-fp.close()
+	fp = open("tokenLines.txt", "r")
+	data = fp.readlines()
+
+	#fp = open("tokensLines_25.txt")
+	buf = [ ]
+	one_big_line = ' '.join(data)
+	data = one_big_line.split(' ')
+	total_tokens = len(data)
+
+	fp2 = open("tokenlines_100.txt", "w")
+	count = 0
+	while count < total_tokens-100:
+		for i in range(0, 101):
+			if data[count+i].find("\n") != -1:
+				buf.append(data[count+i][:-1])
+			else:
+				buf.append(data[count+i])
+		count = count + 100
+		string_to_write = ' '.join(buf)
+		fp2.write(string_to_write+'\n')
+		#print(count)
+		buf=[ ]
+	fp2.close()
+	fp.close()
+
+if __name__ == "__main__":
+    main()
--- tensorflow/rnn.py
+++ tensorflow/rnn.py
@@ -1,116 +1,147 @@
+#!/usr/bin/python
+#
+# RNN script.
+
+from __future__ import print_function
+import os, sys
 import numpy as np
-fp = open("tokens.txt", "r")
-data = fp.readlines()
-dic = {}
-ctr = 0
-for i in data:
-    ctr += 1
-    i = i.split(' ')
-    dic[i[0]] = ctr
-
-vocab_size = ctr
-fp.close()
-
-fp = open("tokenlines_100.txt","r")
-data1 = fp.readlines()
-n = len(data1)
-fp32 = open("tokenlinetest.txt", "r")
-data32 = fp32.readlines()
-n32 = len(data32)
-
-
 import tensorflow as tf
 from tensorflow.python.ops import rnn_cell
 from tensorflow.python.ops import rnn
 
-hm_epochs = 10
-n_classes = vocab_size
-batch_size = 100
-chunk_size = ctr
-n_chunks = 1
-rnn_size = 256
 
-x = tf.placeholder('float', [None, n_chunks, chunk_size])
-y = tf.placeholder('float')
+def main():
 
-def getbatch(i):
-    #for j in range(batch_size):
-    temp = data1[i].split(' ')
-    #print(temp)
-    batch_x = []
-    batch_y = []
-    token_counter = 0
-    for k in temp:
-        temp_x = [0] * vocab_size
-        temp_y = [0] * vocab_size
-        if k not in dic:
-            k = "<RARE_TOKEN>"
-        #print(k)
+    # Read in all required files
+    print("Reading in dictionary file...")
+    fp = open("tokens.txt", "r")
+    data = fp.readlines()
+    dic = {}
+    ctr = 0
+    for i in data:
+        ctr += 1
+        i = i.split(' ')
+        dic[i[0]] = ctr
 
-        if token_counter < batch_size:
-            temp_x[dic[k]-1] += 1
-            batch_x.append(temp_x)
-            if token_counter > 0:
+    vocab_size = ctr
+    fp.close()
+
+
+    print("Reading in training file...")
+    fp = open("tokenlines_100.txt","r")
+    trainingFileLines = fp.readlines()
+    n = len(trainingFileLines)
+
+
+    print("Reading in testing file...")
+    fp32 = open("tokenlinetest.txt", "r")
+    testingFileLines = fp32.readlines()
+    n32 = len(testingFileLines)
+
+
+    # ARCHITECTURAL PARAMETERS
+    hm_epochs = 10
+    n_classes = vocab_size
+    batch_size = 100
+    chunk_size = ctr
+    n_chunks = 1
+    rnn_size = 256
+
+    x = tf.placeholder('float', [None, n_chunks, chunk_size])
+    y = tf.placeholder('float')
+
+    def getbatch(i):
+        #for j in range(batch_size):
+        temp = trainingFileLines[i].split(' ')
+        #print(temp)
+        batch_x = []
+        batch_y = []
+        token_counter = 0
+        for k in temp:
+            temp_x = [0] * vocab_size
+            temp_y = [0] * vocab_size
+            if k not in dic:
+                k = "<RARE_TOKEN>"
+            #print(k)
+
+            if token_counter < batch_size:
+                temp_x[dic[k]-1] += 1
+                batch_x.append(temp_x)
+                if token_counter > 0:
+                    temp_y[dic[k]-1] += 1
+                    batch_y.append(temp_y)
+                token_counter = token_counter + 1
+            else:
                 temp_y[dic[k]-1] += 1
                 batch_y.append(temp_y)
-            token_counter = token_counter + 1
-        else:
-            temp_y[dic[k]-1] += 1
-            batch_y.append(temp_y)
-    '''fbatch_x = []
-    fbatch_y = []
-    for l in batch_x:
-        fbatch_x += l
-    for m in batch_y:
-        fbatch_y += m
-    return np.array(fbatch_x).reshape(1, 87600), np.array(fbatch_y).reshape(1, 87600)'''
-    return np.array(batch_x), np.array(batch_y)
+        '''fbatch_x = []
+        fbatch_y = []
+        for l in batch_x:
+            fbatch_x += l
+        for m in batch_y:
+            fbatch_y += m
+        return np.array(fbatch_x).reshape(1, 87600), np.array(fbatch_y).reshape(1, 87600)'''
+        return np.array(batch_x), np.array(batch_y)
 
-def recurrent_neural_network(x):
-    layer = {'weights':tf.Variable(tf.random_normal([rnn_size, n_classes])),
-                      'biases':tf.Variable(tf.random_normal([n_classes]))}
+    def recurrent_neural_network(x):
+        layer = {'weights':tf.Variable(tf.random_normal([rnn_size, n_classes])),
+                          'biases':tf.Variable(tf.random_normal([n_classes]))}
 
-    x = tf.transpose(x, [1,0,2])
-    x = tf.reshape(x, [-1, chunk_size])
-    x = tf.split(0, n_chunks, x)
+        x = tf.transpose(x, [1,0,2])
+        x = tf.reshape(x, [-1, chunk_size])
+        x = tf.split(0, n_chunks, x)
 
-    lstm_cell = rnn_cell.BasicLSTMCell(rnn_size)
-    outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
+        lstm_cell = rnn_cell.BasicLSTMCell(rnn_size)
+        outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
 
-    output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
+        output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
 
-    return output
+        return output
 
-def train_neural_network(x):
-    prediction = recurrent_neural_network(x)
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
-    
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
+    def train_neural_network(x):
+        prediction = recurrent_neural_network(x)
+        cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+        optimizer = tf.train.AdamOptimizer().minimize(cost)
+        
+        with tf.Session() as sess:
+            sess.run(tf.initialize_all_variables())
 
-        for epoch in range(hm_epochs):
-            epoch_loss = 0
-            for i in range(len(data1)):
-                epoch_x, epoch_y = getbatch(i)
-                #print(epoch_x.shape, batch_size, n_chunks , chunk_size)
-                epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
-                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-                epoch_loss += c
-                #print (i)
-            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss/1000)
+            for epoch in range(hm_epochs):
+                
+                print("Epoch: " + str(epoch))
 
-        for i in range(len(data32)):
-                epoch_xx, epoch_yy = getbatch(i)
-                #print(epoch_x.shape, batch_size, n_chunks , chunk_size)
-                epoch_xx = epoch_xx.reshape((batch_size, n_chunks, chunk_size))
-            
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+                epoch_loss = 0
+                for i in range(len(trainingFileLines)):
+                    if (i % 50 == 0):
+                        print("Training: [" + str(i) + "/" + str(len(trainingFileLines)) + "] [" + ("%.2f" % ((i*1.0)/(len(trainingFileLines)*1.0)*100)) + "%]", end="\r")
+                        sys.stdout.flush()
 
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:',accuracy.eval({x:epoch_xx, y:epoch_yy}))
+                    epoch_x, epoch_y = getbatch(i)
+                    #print(epoch_x.shape, batch_size, n_chunks , chunk_size)
+                    epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
+                    _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                    epoch_loss += c
+                    #print (i)
+                print('\nEpoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss/1000)
 
-train_neural_network(x)
+            for i in range(len(testingFileLines)):
+                    if (i % 50 == 0):
+                        print("Testing: [" + str(i) + "/" + str(len(testingFileLines)) + "] [" + ("%.2f" % ((i*1.0)/(len(testingFileLines)*1.0)*100)) + "%]", end="\r")
+                        sys.stdout.flush()
+                    epoch_xx, epoch_yy = getbatch(i)
+                    #print(epoch_x.shape, batch_size, n_chunks , chunk_size)
+                    epoch_xx = epoch_xx.reshape((batch_size, n_chunks, chunk_size))
+                
+            correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
 
-fp.close()
-fp32.close()
+            accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+            print('Accuracy:',accuracy.eval({x:epoch_xx, y:epoch_yy}))
+
+    train_neural_network(x)
+
+    fp.close()
+    fp32.close()
+
+
+if __name__ == "__main__":
+    main()
